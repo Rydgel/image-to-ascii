6387c60786c132221655a080cf4f51f4e16a5a1d
Jérôme Mahuet <jerome.mahuet@gmail.com>
2016-09-27 13:23:29 +0200

    adding cols+scale

--- src/cmd.rs
+++ src/cmd.rs
@@ -5,6 +5,8 @@ pub struct Command {
     ascii_type: AsciiType,
     source: String,
     output: String,
+    scale: f32,
+    cols: i32,
 }
 
 impl Command {
@@ -19,6 +21,14 @@ impl Command {
     pub fn output(&self) -> &str {
         &self.output
     }
+
+    pub fn scale(&self) -> &f32 {
+        &self.scale
+    }
+
+    pub fn cols(&self) -> &i32 {
+        &self.cols
+    }
 }
 
 pub fn read_command() -> Command {
@@ -26,15 +36,19 @@ pub fn read_command() -> Command {
         (version: "0.1.0")
         (author: "Jérôme Mahuet <jerome.mahuet@gmail.com>")
         (about: "Convert an image to ascii art.")
-        (@arg type: -t --type +required +takes_value "Sets the type - simple|complex")
+        (@arg type: -t --type +takes_value "Sets the type - simple|complex")
         (@arg src: -s --src +required +takes_value "Path of the source image")
         (@arg output: -o --output +required +takes_value "Path of the output file")
+        (@arg scale: --scale +takes_value "Font scaling")
+        (@arg cols: --cols +takes_value "Number of columns")
     ).get_matches();
 
     Command {
         ascii_type: read_type(matches.value_of("type").unwrap_or("simple")),
         source: matches.value_of("src").unwrap().to_string(),
         output: matches.value_of("output").unwrap().to_string(),
+        scale: value_t!(matches.value_of("scale"), f32).unwrap_or(0.43),
+        cols: value_t!(matches.value_of("cols"), i32).unwrap_or(80),
     }
 }
 
